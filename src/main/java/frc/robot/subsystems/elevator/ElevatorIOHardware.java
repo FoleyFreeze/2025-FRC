78d20f5ff0cd7c3873adca2c44690d4e97f6dae1
25estyf <25estyf>
2025-01-18 14:43:13 -0500

    elevator development

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOHardware.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOHardware.java
@@ -1,16 +1,14 @@
 package frc.robot.subsystems.elevator;
 
-import java.util.Queue;
-
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
-import frc.robot.subsystems.drive.PhoenixOdometryThread;
 
 public class ElevatorIOHardware implements ElevatorIO {
 
@@ -21,29 +19,43 @@ public class ElevatorIOHardware implements ElevatorIO {
 
   private final TalonFX elevatorTalon;
 
-  public ElevatorIOHardware(){
+  public ElevatorIOHardware() {
     elevatorTalon = new TalonFX(0, "*");
 
     elevatorPosition = elevatorTalon.getPosition();
     elevatorVelocity = elevatorTalon.getVelocity();
     elevatorAppliedVolts = elevatorTalon.getMotorVoltage();
     elevatorCurrent = elevatorTalon.getStatorCurrent();
-
   }
 
   @Override
   public void updateInputs(ElevatorIOInputs inputs) {
-    var elevatorStatus = BaseStatusSignal.refreshAll(elevatorPosition, elevatorVelocity, elevatorAppliedVolts, elevatorCurrent);
-  
+    var elevatorStatus =
+        BaseStatusSignal.refreshAll(
+            elevatorPosition, elevatorVelocity, elevatorAppliedVolts, elevatorCurrent);
 
-    
-
-    //inputs.elevatorConnected = elevatorConnectedDebounce.calculate(elevatorStatus.isOK());
+    // inputs.elevatorConnected = elevatorConnectedDebounce.calculate(elevatorStatus.isOK());
     inputs.elevatorPosition = elevatorPosition.getValue();
     inputs.elevatorVelocity = elevatorVelocity.getValue();
     inputs.elevatorAppliedVolts = elevatorAppliedVolts.getValue();
     inputs.elevatorCurrent = elevatorCurrent.getValue();
+  }
+
+  @Override
+  public void setElevatorVelocity(AngularVelocity velocityRadPerSec) {
+    
+  }
+
+  @Override
+  public void setElevatorVolts(Voltage volts) {
 
   }
+
+  @Override
+  public void setElevatorPosition(Angle motorPosition) {
+
+  }
+
+
 
 }
