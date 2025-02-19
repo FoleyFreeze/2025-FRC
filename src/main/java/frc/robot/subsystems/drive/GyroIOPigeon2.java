edf86909d004aeba88598852d71870446775a26d
28EMcCallum <28EMcCallum@BishopFoley.org>
2025-02-18 21:12:50 -0500

    working on filtering vision data only allow metatag1 when angle needs to be updated

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -22,6 +22,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearAcceleration;
 import frc.robot.generated.TunerConstants;
 import java.util.Queue;
 
@@ -35,22 +36,32 @@ public class GyroIOPigeon2 implements GyroIO {
     private final Queue<Double> yawPositionQueue;
     private final Queue<Double> yawTimestampQueue;
     private final StatusSignal<AngularVelocity> yawVelocity = pigeon.getAngularVelocityZWorld();
+    private final StatusSignal<LinearAcceleration> accelX = pigeon.getAccelerationX();
+    private final StatusSignal<LinearAcceleration> accelY = pigeon.getAccelerationY();
+    private final StatusSignal<LinearAcceleration> accelZ = pigeon.getAccelerationZ();
 
     public GyroIOPigeon2() {
         pigeon.getConfigurator().apply(new Pigeon2Configuration());
         pigeon.getConfigurator().setYaw(0.0);
         yaw.setUpdateFrequency(Drive.ODOMETRY_FREQUENCY);
-        yawVelocity.setUpdateFrequency(50.0);
+
+        BaseStatusSignal.setUpdateFrequencyForAll(50.0, yawVelocity, accelX, accelY, accelZ);
         pigeon.optimizeBusUtilization();
+
         yawTimestampQueue = PhoenixOdometryThread.getInstance().makeTimestampQueue();
         yawPositionQueue = PhoenixOdometryThread.getInstance().registerSignal(pigeon.getYaw());
     }
 
     @Override
     public void updateInputs(GyroIOInputs inputs) {
-        inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
+        inputs.connected =
+                BaseStatusSignal.refreshAll(yaw, yawVelocity, accelX, accelY, accelZ)
+                        .equals(StatusCode.OK);
         inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
         inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
+        inputs.accelX = accelX.getValueAsDouble();
+        inputs.accelX = accelY.getValueAsDouble();
+        inputs.accelX = accelZ.getValueAsDouble();
 
         inputs.odometryYawTimestamps =
                 yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
--- src/main/java/frc/robot/subsystems/wrist/WristCals.java
+++ src/main/java/frc/robot/subsystems/wrist/WristCals.java
@@ -8,9 +8,9 @@ import edu.wpi.first.units.measure.Angle;
 public class WristCals {
     double wristLength = 6;
     double g1 = 10.935;
-    double g2 = 30.0 / 18.0;
+    double g2 = 30.0 / 19.0;
     double g3 = 40.0 / 30.0;
-    double gearRatio = 24.3; // g1 * g2 * g3;
+    double gearRatio = g1 * g2 * g3;
     public double gearRatioToAbsEncoder = g2 * g3;
     double absEncOffset = 0.6643;
     double startEncVal = -59;
