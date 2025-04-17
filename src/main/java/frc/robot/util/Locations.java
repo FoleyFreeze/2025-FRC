3790be9bc1d4c71cfe61f4a6d22df99cfaa61c59
Roboprog <roboprog@BishopFoley.org>
2025-04-17 16:04:58 -0400

    formatting

--- src/main/java/frc/robot/util/Locations.java
+++ src/main/java/frc/robot/util/Locations.java
@@ -89,9 +89,7 @@ public class Locations {
 
     public static Transform2d halfRobotLevel1Right =
             new Transform2d(
-                    Units.inchesToMeters(0),
-                    Units.inchesToMeters(52),
-                    Rotation2d.fromDegrees(83));
+                    Units.inchesToMeters(0), Units.inchesToMeters(52), Rotation2d.fromDegrees(83));
 
     public static Transform2d supercycleBackup =
             new Transform2d(Units.inchesToMeters(-8), 0, Rotation2d.kZero);
